68a9617e21c0ded1998c98aa1d393a2a9de360c0
Nicky-babe <azharchy472100@gmail.com>
2025-09-08 00:25:42 +0600

    Update Mentionadmin.js

--- Script/commands/Mentionadmin.js
+++ Script/commands/Mentionadmin.js
@@ -10,7 +10,7 @@ module.exports.config = {
 };
 
 module.exports.handleEvent = function({ api, event }) {
- const adminIDs = ["61575698041722", "100001039692046", "61576394315037"].map(String);
+ const adminIDs = ["61578598505328", "61578598505328", "61579956563449"].map(String);
  
  if (adminIDs.includes(String(event.senderID))) return;
 
